26cea88a4257f12d239147dac94797bd80bd5a6a
jayant-swami <jayantjayesh@gmail.com>
2019-10-26 00:55:35 +0530

    Changed validation object in userValidation Middleware || Added login route and jwt token signing

--- middleware/userValidation.js
+++ middleware/userValidation.js
@@ -3,17 +3,19 @@ const userModel = require("../models/userModel");
 const userValidation = (req, res, next) => {
   let validation = {
     status: "SUCCESS",
-    user_name: "",
-    email: "",
-    password: "",
-    first_name: "",
-    last_name: ""
+    error: {
+      user_name: "",
+      email: "",
+      password: "",
+      first_name: "",
+      last_name: ""
+    }
   };
 
   // Check User Name
   const userNameRegex = /^[a-z0-9_-]{3,16}$/;
   if (!userNameRegex.test(req.body.user_name)) {
-    validation.user_name =
+    validation.error.user_name =
       "User Name must be 3 to 16 characters long. Only lowercase alphanumeric, underscores(_) and hyphens(-) are allowed";
     validation.status = "FAIL";
   }
@@ -21,27 +23,27 @@ const userValidation = (req, res, next) => {
   // Check Email
   const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (!emailRegex.test(String(req.body.email).toLowerCase())) {
-    validation.email = "Email is not valid";
+    validation.error.email = "Email is not valid";
     validation.status = "FAIL";
   }
 
   //   Check Password
   const passwordRegex = /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/;
   if (!passwordRegex.test(req.body.password)) {
-    validation.password =
+    validation.error.password =
       "Password should have 1 lowercase letter, 1 uppercase letter, 1 number, and be at least 8 characters long";
     validation.status = "FAIL";
   }
 
   const firstNameRegexp = /^[a-zA-Z]{2,30}$/;
   if (req.body.first_name && !firstNameRegexp.test(req.body.first_name)) {
-    validation.first_name = "Only alphabets are allowed. Length: 2 to 30";
+    validation.error.first_name = "Only alphabets are allowed. Length: 2 to 30";
     validation.status = "FAIL";
   }
 
   const lastNameRegexp = /^[a-zA-Z]{1,30}$/;
   if (req.body.last_name && !lastNameRegexp.test(req.body.last_name)) {
-    validation.last_name = "Only alphabets are allowed. Length: 1 to 30";
+    validation.error.last_name = "Only alphabets are allowed. Length: 1 to 30";
     validation.status = "FAIL";
   }
 
@@ -72,22 +74,17 @@ const userValidation = (req, res, next) => {
   Promise.all([userPromise, emailPromise])
     .then(vals => {
       if (vals[0] === 1) {
-        validation.user_name = "User already registered";
+        validation.error.user_name = "User already registered";
         validation.status = "FAIL";
       }
       if (vals[1] === 1) {
-        validation.email = "Email already registered";
+        validation.error.email = "Email already registered";
         validation.status = "FAIL";
       }
 
-      if (validation.status === "SUCCESS") {
+
         req.validation = validation;
-        next();
-      } else {
-        req.validation = validation;
-        next();
-      }
-    })
+        next();    })
     .catch(err => {
       console.log(err);
     });
--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const userValidation = require("../../middleware/userValidation");
 const userModel = require("../../models/userModel");
-const bcrypt= require("bcrypt");
+const bcrypt = require("bcrypt");
+const config = require("../../config/config");
+const jwt = require("jsonwebtoken");
 
-router.post("/", userValidation, (req, res) => {
+router.post("/register", userValidation, (req, res) => {
   if (req.validation.status === "SUCCESS") {
     let newUser = userModel({
       user_name: req.body.user_name,
@@ -34,6 +36,92 @@ router.post("/", userValidation, (req, res) => {
     });
   } else {
     res.send(req.validation);
+  }
+});
+
+router.post("/login", (req, res) => {
+  let response = {
+    status: "SUCCESS",
+    token: "",
+    user: {
+      user_name: req.body.user_name,
+      email: "",
+      first_name: "",
+      last_name: "",
+      added_on: ""
+    },
+    error: {
+      user_name: "",
+      password: ""
+    }
+  };
+
+  if (!req.body.user_name) {
+    response.error.user_name = "User Name is required";
+    response.status = "FAIL";
+  }
+  if (!req.body.password) {
+    response.error.password = "Password is required";
+    response.status = "FAIL";
+  }
+
+  if (response.status === "SUCCESS") {
+    userModel
+      .findOne(
+        { user_name: req.body.user_name },
+        "email first_name last_name password"
+      )
+      .then(val => {
+        if (val) {
+          console.log(val);
+          bcrypt
+            .compare(req.body.password, val.password)
+            .then(value => {
+              console.log("value");
+              if (value) {
+                // sign jwt token
+                jwt.sign(
+                  { id: val.id },
+                  config.jwt.jwtSecret,
+                  (err, tokenVal) => {
+                    if (err) {
+                      res
+                        .status(500)
+                        .send(
+                          "Server Error: Error Occured while signing token"
+                        );
+                    } else {
+                      (response.token = tokenVal),
+                        (response.user.email = val.email),
+                        (response.user.first_name = val.first_name),
+                        (response.user.last_name = val.last_name),
+                        (response.user.added_on = val.added_on);
+                        res.send(response);
+                    }
+                  }
+                );
+              } else {
+                response.error.password = "Incorrect Password";
+                response.status = "FAIL";
+                res.send(response);
+              }
+            })
+            .catch(err => {
+              console.log(err);
+              res
+                .status(500)
+                .send("Server Error: Error Occured while comparing pass");
+            });
+        } else {
+          response.error.user_name = "User not registered";
+          response.status = "FAIL";
+          res.send(response)
+        }
+      })
+      .catch((err)=> {
+          console.log(err);
+          res.status(500).send("Server Error: An error occured while user search")
+      });
   }
 });
 
